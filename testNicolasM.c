419cf1c0aa06406998a587547f74146d7f8b34dd
nicolas-malarge <nicolas.malarge@edu.esiee.fr>
2026-03-11 14:54:27 +0000

    structs

---
+++ testNicolasM.c
@@ -0,0 +1,22 @@
+struct Serveur
+{
+    /* data */
+    int idServeur;
+    char nom;
+};
+
+struct Client
+{
+    /* data */
+    int idClient;
+    char nom;
+    char prenom;
+};
+
+
+
+int main(){
+
+    struct Serveur serveur1;
+    return 0;
+}
