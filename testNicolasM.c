81ce880f4c54a5de83f4338df935280f73be93fc
nicolas-malarge <nicolas.malarge@edu.esiee.fr>
2026-03-11 16:53:21 +0000

    structure

--- testNicolasM.c
+++ testNicolasM.c
@@ -1,22 +1,26 @@
-struct Serveur
-{
-    /* data */
+#include <stdio.h>
+#include <string.h> 
+
+struct Serveur {
     int idServeur;
-    char nom;
+    char nom[30];
 };
 
-struct Client
-{
-    /* data */
+struct Client {
     int idClient;
-    char nom;
-    char prenom;
+    char nom[20];
+    char prenom[20];
 };
 
+int main() {
+    struct Serveur s1;
+    struct Serveur s2;
 
+    s1.idServeur = 1;
+    strcpy(s1.nom, "restaurant 1");
 
-int main(){
+    printf("Nom serveur : %s\n", s1.nom);
+    printf("Identifiant serveur : %d\n", s1.idServeur);
 
-    struct Serveur serveur1;
     return 0;
 }
