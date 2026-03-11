edccaa6a9b0084b508bbde93b05a447ee97d6417
nicolas-malarge <nicolas.malarge@edu.esiee.fr>
2026-03-11 17:10:03 +0000

    a_jour

--- testNicolasM.c
+++ testNicolasM.c
@@ -15,6 +15,7 @@ struct Client {
 int main() {
     struct Serveur s1;
     struct Serveur s2;
+    struct Client c1;
 
     s1.idServeur = 1;
     strcpy(s1.nom, "restaurant 1");
@@ -22,10 +23,17 @@ int main() {
     s2.idServeur = 2;
     strcpy(s2.nom, "restaurant 2");
 
+    c1.idClient = 1;
+    strcpy(c1.nom, "MALARGE");
+    strcpy(c1.prenom, "Nicolas");
+
     printf("Nom serveur : %s\n", s1.nom);
     printf("Identifiant serveur : %d\n", s1.idServeur);
 
     printf("Nom serveur : %s\n", s2.nom);
     printf("Identifiant serveur : %d\n", s2.idServeur);
+
+    printf("Nom client : %s\n", c1.nom);
+    printf("Prenom client : %s\n", c1.prenom);
     return 0;
 }
