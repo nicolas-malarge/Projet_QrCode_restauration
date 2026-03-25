7eaa7e50596c5485ea0fe1877226e6d160bb1c27
nicolas-malarge <nicolas.malarge@edu.esiee.fr>
2026-03-25 11:45:23 +0000

    amelioration affichage

--- data.c
+++ data.c
@@ -19,20 +19,14 @@ struct Plat {
 };
 
 void afficherMenu(struct Plat pPlat){
-    printf("Plat : %s ........ Prix : %f euros\n", pPlat.plat, pPlat.prix);
-
-    //int j;
-    //    while (pPlat.plat[j]!='\0'){
-    //        printf("%c", pPlat.plat[j]);
-    //        j++;
-    //    }
-    //printf("%s\n", pPlat.plat);
+    //printf("Menu %s \n", );
+    printf("%s......... Prix : %f euros\n", pPlat.plat, pPlat.prix);
 }
 
-void ajoutStruct(struct Plat pPlat[], int pId, char pNomPlat, double pPrix){
+void ajoutStruct(struct Plat pPlat[], int pId, char* pNomPlat, double pPrix){
     struct Plat vPlat;
     vPlat.idPlat = pId;
-    vPlat.plat[30] = pNomPlat;
+    strcpy(vPlat.plat, pNomPlat); 
     vPlat.prix = pPrix;
     pPlat[pId] = vPlat;
 }
@@ -43,13 +37,11 @@ int main() {
     struct Client c1;
     struct Plat menu1[100];
 
-    char char1[30] = {'L','e',' ','C', 'l', 'a', 's', 's', 'i', 'q', 'u', 'e'};
-    printf("%s", char1);
-    ajoutStruct(menu1, 1, char1, 14.0);
+    ajoutStruct(menu1, 1, "Le Classique..", 14.0);
     ajoutStruct(menu1, 2, "Burger Country", 16.0);
     ajoutStruct(menu1, 3, "Burger Buffalo", 16.0);
-    ajoutStruct(menu1, 4, "Le Royal", 18.0);
-    ajoutStruct(menu1, 5, "Le BurKid", 8.0);
+    ajoutStruct(menu1, 4, "Le Royal......", 18.0);
+    ajoutStruct(menu1, 5, "Le BurKid.....", 8.0);
 
     s1.idServeur = 1;
     strcpy(s1.nom, "Burger Happy");
