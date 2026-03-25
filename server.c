e35f0c3d63ea5746c50a438ef1db5f4c85ce4849
Le MILINAIRE Nicolas <nicolas.lemilinaire@edu.esiee.fr>
2026-03-25 17:18:31 +0000

    server.c

--- server.c
+++ server.c
@@ -10,22 +10,27 @@
 // for mkfifo
 // for read , write
 # define BUFFER_SIZE 512
-# define ServerE "ServerE.tube"
-# define ClientS "clientS.tube"
+# define S2C "S2C.tube"
+# define C2S "C2S.tube"
+
+# define S2D "S2D.tube"
+# define D2S "D2S.tube"
 
 
 int main () {
     char buffer [ BUFFER_SIZE ];
 
-    mkfifo(ServerE, 0666);
-    mkfifo(ClientS, 0666);
+    mkfifo(S2C, 0666);
+    mkfifo(C2S, 0666);
+    mkfifo(S2D, 0666);
+    mkfifo(D2S, 0666);
 
     printf("Serveur en attente du client...\n");
 
     
-    int fd_read = open(ServerE, O_RDONLY);
+    int fd_read = open(S2C, O_RDONLY);
     if ( fd_read == -1) {
-        printf ( " Ne peut ouvrir ’%s ’\n " , ServerE ) ;
+        printf ( " Ne peut ouvrir ’%s ’\n " , S2C ) ;
         return 0;
     }
     read(fd_read, buffer, sizeof(&buffer));
@@ -35,7 +40,7 @@ int main () {
     close(fd_read);
 
     
-    int fd_write = open(ClientS, O_WRONLY);
+    int fd_write = open(C2S, O_WRONLY);
 
     char *menu = "Menu:\n1. Le classique\n2. Burger country\n3. Buffalo\n4. Le Royal \n5. Le Burkid\n";
     write(fd_write, menu, strlen(menu));
