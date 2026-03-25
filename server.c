b19796df5e7f86f8a4ecd3ac0a337ba7240412d3
Le MILINAIRE Nicolas <nicolas.lemilinaire@edu.esiee.fr>
2026-03-25 13:32:05 +0000

    updated server.c

--- server.c
+++ server.c
@@ -1,44 +1,41 @@
-# include <netinet/in.h> // for sockaddr_in , htons , INADDR_ANY , IPPROTO_TCP
-# include <stdio.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+// for open , O_RDONLY , O_WRONLY
 // for printf
-# include <sys/socket.h> // for accept , bind , listen , socket , AF_INET , SOCK_ ...
-# define NB_CLIENTS 2
-# define IP INADDR_ANY
-# define PORT 8080
-# define BACKLOG 3
+// for mkfifo
+// for read , write
+# define BUFFER_SIZE 512
+# define ServerE "ServerE.tube"
+# define ClientS "clientS.tube"
+
 
 int main () {
-    int clients [ NB_CLIENTS ];
-    // Initialise adresse
-    struct sockaddr_in adresse ;
-    adresse.sin_family = AF_INET ;
-    // IPv4
-    adresse.sin_addr.s_addr = IP ;
-    // L adresse eccoute
-    adresse.sin_port = htons ( PORT ) ; // Le port ecoute
-    // Creation de la socket
-    int fdsocket = socket( AF_INET , SOCK_STREAM ,  IPPROTO_TCP ) ; // TCP
-    if ( fdsocket == -1) {
-        return 0;
-    }
-    // Attache la socket au port
-    int result = bind (fdsocket , (struct sockaddr*) &adresse , sizeof(adresse ));
-    if ( result != 0) {
-        return 0;
-    }
-    // Ecoute sur la socket
-    result = listen ( fdsocket , BACKLOG ) ;
-    if ( result != 0) {
-        return 0;
-    }
-    struct sockaddr_in client_adresse ;
-    int taille = sizeof ( client_adresse ) ;
-    // Attend une connexion
-    int client = accept( fdsocket , ( struct sockaddr *) &client_adresse , ( socklen_t *) &taille );
-    if ( client == -1) {
-        return 0;
-}
-    printf (" Connexion \n") ;
-    close ( client ) ;
-    close ( fdsocket ) ;
+    char buffer [ BUFFER_SIZE ];
+
+    mkfifo(ServerE, 0666);
+    mkfifo(ClientS, 0666);
+
+    printf("Serveur en attente du client...\n");
+
+    
+    int fd_read = open(ServerE, O_RDONLY);
+    read(fd_read, buffer, sizeof(buffer));
+
+    printf("Commande reçue : %s\n", buffer);
+
+    close(fd_read);
+
+    
+    int fd_write = open(ClientS, O_WRONLY);
+
+    char *menu = "Menu:\n1. Pizza\n2. Burger\n3. Salade\n";
+    write(fd_write, menu, sizeof(menu) + 1);
+
+    close(fd_write);
+
+    return 0;
 }
