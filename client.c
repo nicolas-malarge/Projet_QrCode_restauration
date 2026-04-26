dea9c34ec415e075bba688f0814484084078009e
Le MILINAIRE Nicolas <nicolas.lemilinaire@edu.esiee.fr>
2026-04-26 22:24:02 +0200

    Increase buffer size and update menu options
    Increased buffer size for reading and writing operations, updated menu options, and improved read functionality.

--- client.c
+++ client.c
@@ -10,17 +10,17 @@
 // for printf
 // for mkfifo
 // for read , write
-# define BUFFER_SIZE 512
+# define BUFFER_SIZE 4096
 # define S2C "S2C.tube"
 # define C2S "C2S.tube"
 
 int main () {
-    char buffer[512];
+    char buffer[BUFFER_SIZE];
     char choix[10];
 
     printf("1 : Menu Burger Happy\n");
     printf("2 : Menu Happy Sushi\n");
-    printf("3 : Menu Happy Sushi\n");
+    printf("3 : Menu Pizza Happy\n");
     printf("Entrez le numero de votre choix : ");
     
     if (fgets(choix, sizeof(choix), stdin) == NULL) {
@@ -30,13 +30,16 @@ int main () {
     choix[strcspn(choix, "\n")] = 0;
 
     int fd_out = open(C2S, O_WRONLY);
-    char *msg = choix;
-    write(fd_out, msg, strlen(msg) + 1);
+    //char *msg = choix;
+    write(fd_out, choix, strlen(choix) + 1);
     close(fd_out);
 
     int fd_in = open(S2C, O_RDONLY);
-    read(fd_in, buffer, sizeof(buffer));
+    //read(fd_in, buffer, sizeof(buffer));
+    ssize_t bytes_read = read(fd_in, buffer, BUFFER_SIZE - 1);
     printf("Menu reçu :\n%s\n", buffer);
     close(fd_in);
+
+
     return 0;
 }
