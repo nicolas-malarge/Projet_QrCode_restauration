2359baade9b9d839a26b6615a3b19639c6d322b7
Le MILINAIRE Nicolas <nicolas.lemilinaire@edu.esiee.fr>
2026-05-05 13:30:55 +0200

    Enhance comments in traiterRequete function
    Added comments for clarity and improved code readability.

--- data.c
+++ data.c
@@ -13,14 +13,14 @@ typedef struct {
     char requete[512];
 } Information;
 
-void* traiterRequete(void* pRequete) {
+void* traiterRequete(void* pRequete) { //traite la requète envoyée par le client.
     Information* data = (Information*)pRequete;
     char buffer_reponse[512] = "";
     FILE *fichier;
 
     printf("Traitement de la requête : %s\n", data->requete);
 
-    if(strcmp(data->requete, "1234 1234 0001") == 0) { 
+    if(strcmp(data->requete, "1234 1234 0001") == 0) { //on envoie le menu du restaurant correspondant.
         fichier = fopen("menu_burger.txt", "r");
     }else if(strcmp(data->requete, "1234 1234 0002") == 0) {
         fichier = fopen("menu_sushi.txt", "r");
@@ -30,16 +30,18 @@ void* traiterRequete(void* pRequete) {
         fichier = fopen("codes_erreur.txt", "r");
     }
 
-    if(fichier != NULL) {
+    if(fichier != NULL) { //le bon fichier récupéré, on vérifie s'il n'est pas vide.
         char ligne[256];
+        //on récupère et lie les lignes contenues dans le fichier .txt.
         while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
             strcat(buffer_reponse, ligne);
         }
-        fclose(fichier);
-    }else{
+        fclose(fichier); //on ferme le fichier
+    }else{ //si le fichier est vide, on renvoie un message d'erreur
         strcpy(buffer_reponse, "Erreur : Fichier introuvable sur le serveur.\n");
     }
 
+    //on envoie le contenu du fichier à travers le tube vers le serveur puis on le ferme.
     int fd_out = open(D2S, O_WRONLY);
     write(fd_out, buffer_reponse, strlen(buffer_reponse) + 1);
     close(fd_out);
@@ -51,13 +53,16 @@ void* traiterRequete(void* pRequete) {
 int main() {
     char buffer[512];
 
+    //création des tubes
     mkfifo(S2D, 0666);
     mkfifo(D2S, 0666);
 
     printf("Serveur data en attente de requetes...\n");
 
+    //permet de laisser tourner data même après la gestion des clients.
     while(1) {
         int fd_in = open(S2D, O_RDONLY);
+        //lit la sortie du pipe venant de server et alloue la mémoire necessaire.
         if(read(fd_in, buffer, sizeof(buffer)) > 0) {
             
             Information* vReponse = malloc(sizeof(Information));
